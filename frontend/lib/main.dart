c8099be6a2acdc29a1a979305fec913133fb017b
jpsantosss <jpedrosantosoliv@gmail.com>
2025-10-01 18:22:15 -0300

    updates

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,10 +1,48 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'dart:html' as html;
+import 'dart:ui_web' as ui_web;
 import 'package:flutter/material.dart';
 import 'package:flutter_tcc/presentation/screens/login/login_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() async {
+Future<void> main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    final completer = Completer<void>();
+
+    const String apiKey = String.fromEnvironment('WEB_MAPS_API_KEY');
+    if (apiKey == '') {
+      print('ERRO: A chave da API do Google Maps para Web não foi definida.');
+      return;
+    }
+
+    ui_web.platformViewRegistry.registerViewFactory(
+      'google-map',
+      (int viewId) =>
+          html.IFrameElement()
+            ..id = 'map-iframe'
+            ..style.border = 'none',
+    );
+
+    final script =
+        html.ScriptElement()
+          ..id = 'google-maps-script'
+          ..src = 'https://maps.googleapis.com/maps/api/js?key=$apiKey'
+          ..async = true
+          ..defer = true;
+
+    script.onLoad.listen((_) {
+      completer.complete();
+    });
+
+    html.document.head!.children.add(script);
+
+    await completer.future;
+  }
   await initializeDateFormatting('pt_BR', null);
   runApp(const MyApp());
 }
@@ -21,9 +59,7 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: [
-        Locale('pt', 'BR'),
-      ],
+      supportedLocales: [Locale('pt', 'BR')],
       home: LoginScreen(),
     );
   }
