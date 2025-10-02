d0fb1af21d612557437eb76e449d161a85a58382
jpsantosss <jpedrosantosoliv@gmail.com>
2025-10-02 14:34:49 -0300

    update ordem de servico model

--- frontend/lib/data/models/ordem_servico.dart
+++ frontend/lib/data/models/ordem_servico.dart
@@ -8,23 +8,19 @@ class OrdemServico {
   final String ativo;
   final DateTime inicio;
   final DateTime fim;
-  final Color cor;
   final String usuarioSolicitante;
   final DateTime dataCriacao;
   final String tipoManutencao;
   final StatusOS status;
-  final PrioridadeOS prioridade;
 
   OrdemServico({
     required this.titulo,
     required this.ativo,
     required this.inicio,
     required this.fim,
-    required this.cor,
     required this.usuarioSolicitante,
     required this.dataCriacao,
     required this.tipoManutencao,
     required this.status,
-    required this.prioridade,
   });
 }
